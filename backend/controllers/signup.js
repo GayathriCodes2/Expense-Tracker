ad7f88dab8513c2f9a1e329530bbc4055f81ce0e
GayathriCodes2 <gayathricodes2@gmail.com>
2024-05-31 01:00:53 +0530

    backend api for login page

--- backend/controllers/signup.js
+++ backend/controllers/signup.js
@@ -1,35 +1,39 @@
 //controller/signup.js
 const signup = require('../models/signUp');
 
-exports.addsignup = async(req,res)=>{
-    try{
-    console.log("/////////////////",req.body);
-    const data = await signup.create(req.body)
-    res.status(200).send({message: 'Signup successful',data:data});
-    }catch(e){
+exports.addsignup = async (req, res) => {
+    try {
+        console.log("/////////////////", req.body);
+        const data = await signup.create(req.body)
+        res.status(200).send({ message: 'Signup successful', data: data });
+    } catch (e) {
         console.log(e);
-        res.send({message: e.message});
+        res.send({ message: e.message });
     }
 }
 
-exports.login = async(req,res)=>{
+exports.login = async (req, res) => {
     try {
-        const {email,password} = req.body;
-        const data = await signup.findOne({where:{email:email}})
-        console.log(">>>>>>>>>>>",data);
-        if(data!=null){
-            console.log(data.password===password);
-        if(data.password===password){
-            res.status(200).send({message:"Login Successfull"})
-        }else{
-            res.status(200).send({message:"password incorrect"})
+        const { email, password } = req.body;
+        console.log(req.body);
+        const data = await signup.findOne({ where: { email: req.body.email } })
+        console.log(">>>>>>>>>>>", data);
+        if (data != null) {
+            console.log(data.password === password);
+            if (data.password === password) {
+                res.status(200).send({ message: "Login Successfull" })
+            } else {
+                //password incorrect
+                res.status(400).send({ message: "Error" })
+            }
+        } else {
+            //Email does not exist
+            res.status(400).send({ message: "Error" })
         }
-    }else{
-        res.status(400).send({message:"Email does not exist"})
-    }
 
     } catch (error) {
-        res.status(400).send({message:"Email or password does not exist"})
-        console.log(":::::::::::::",error);
+        // Email or password does not exist
+        res.status(400).send({ message: "Error" })
+        console.log(":::::::::::::", error);
     }
 }
